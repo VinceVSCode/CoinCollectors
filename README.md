aea1898c413b6e1787b5e822cdc0fe7769064859
Themis Sarafidis <sarafidis12@gmail.com>
2026-03-01 21:28:44 +0200

    Add initial CHANGELOG and pom.xml files, and create App.java entry point

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CoinCollectors
-The aim of this folder is an application for coin collectors.
+The aim of this folder is an application for coin collectors. That is
+the main idea. Change will be done along the way.
---
+++ src/main/java/com/vincevscode/cointracker/App.java
@@ -0,0 +1,9 @@
+// v0.1.0: Minimal application entry point to verify build/run works.
+package com.vincevscode.cointracker;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Coin Tracker (v0.1.0) - Setup OK");
+        System.out.println(" Hello, Coin Tracker (v0.1.0) world!");
+    }
+}
